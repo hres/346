3a636f6e0a56bccff0b32564f5a7de65365279d4
Romario Chuela <nelsonromario@hotmail.com>
2018-06-20 07:44:36 -0700

    added generic exception handler

---
+++ src/hc/fcdr/rws/except/GenericExceptionMapper.java
@@ -0,0 +1,24 @@
+package hc.fcdr.rws.except;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class GenericExceptionMapper  implements ExceptionMapper<Throwable> {
+  @Override
+  public Response toResponse(Throwable exception) {
+    exception.printStackTrace();
+    Logger.getGlobal().log(
+      Level.SEVERE,
+      exception.getMessage()
+    );
+    return Response.status(Status.INTERNAL_SERVER_ERROR).entity("Internal Server Error").build();
+  }
+}
+
