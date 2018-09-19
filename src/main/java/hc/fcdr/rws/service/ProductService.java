a16afaa704d0cc9c59aceae960ba286f9e1e4ef5
Romario Chuela <nelsonromario@hotmail.com>
2018-09-19 11:55:00 -0700

    fixed keycloak

--- src/main/java/hc/fcdr/rws/service/ProductService.java
+++ src/main/java/hc/fcdr/rws/service/ProductService.java
@@ -1,27 +1,22 @@
 package hc.fcdr.rws.service;
 
 import java.io.IOException;
-import java.security.Principal;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
+
 
 import hc.fcdr.rws.db.DbConnection;
 import hc.fcdr.rws.db.ProductDao;
